d03fa1834685f31d771cd503350911ded84cc96d
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2025-12-03 22:14:00 +0500

    created a showcase file and use a video and svg to mask on each other

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -5,7 +5,7 @@ import { Canvas } from '@react-three/fiber'
 import { Box } from '@react-three/drei'
 import { OrbitControls } from '@react-three/drei'
 import MacbookModel14 from './models/Macbook-14'
-import StudioLights from './StudioLights'
+import StudioLights from './three/StudioLights'
 import ModelSwitcher from './three/ModelSwitcher'
 import { useMediaQuery } from 'react-responsive'
 
